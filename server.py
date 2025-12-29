50466e241195f1d156834eda16ed8ae764b6053b
acoolvoice03-ops <acoolvoice03@gmail.com>
2025-12-29 18:42:33 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+PORT = int(os.environ.get("PORT", 10000))
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+HTTPServer(("", PORT), Handler).serve_forever()
